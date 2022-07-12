b59ab463e5cc5ceaa91f22a38153b51912894c54
smex <cnexxx@gmail.com>
2022-07-13 01:39:37 +0300

    condition task 1st commit

--- cond1.sh
+++ cond1.sh
@@ -2,4 +2,13 @@
 
 IFS=',' read -r -a array <<< "$1"
 
-#Write your code here
+sum=0
+for val in ${array[@]}
+do
+    if [ $((val%2)) -eq 0 ]
+    then
+        sum=$(( $sum+$val ))
+    fi
+done
+
+echo $sum
